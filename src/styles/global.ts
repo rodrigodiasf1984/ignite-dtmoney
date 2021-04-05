eda23bd36361af05b5e358bdad89b53564e261c2
Rodrigo Dias De Figueiredo <rodrigodiasf@gmail.com>
2021-04-05 19:38:23 +0200

    🎨 M- added font Poppins

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,9 +1,17 @@
 import {createGlobalStyle} from 'styled-components';
 
 export const GlobalStyle = createGlobalStyle`
+
 :root{
-  --background: #f0f2f5;
+  --background: #F0F2F5;
+  --red: #E52E4D;
+  --blue: #5429CC;
+  --blue-light: #6933FF;
+  --text-title: #363F5F;
+  --text-body: #969CB3;
+  --shape: #FFFFFF;
 }
+
 *{
   margin:0;
   padding:0;
@@ -24,6 +32,15 @@ html{
 body{
   background: var(--background);
   -webkit-font-smoothing: antialiased;
+}
+
+body, input, textarea, button {
+  font-family: 'Poppins', sans-serif;
+  font-weight: 400;
+}
+
+h1,h2,h3,h4,h5,h6, strong {
+  font-weight: 600;
 }
 
 button {
