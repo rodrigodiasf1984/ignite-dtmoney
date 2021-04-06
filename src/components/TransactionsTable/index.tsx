db35cefe5d4801a7e61b28426102153558b5abd4
Rodrigo Dias De Figueiredo <rodrigodiasf@gmail.com>
2021-04-06 23:34:25 +0200

    🎨 M- added axios and miragejs, to create and cosume a fake-api

--- src/components/TransactionsTable/index.tsx
+++ src/components/TransactionsTable/index.tsx
@@ -1,7 +1,18 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { api } from "../../services/api";
 import { Container } from "./styles";
 
-export function TransactionsTable(){
+export function TransactionsTable() {
+  const [transactions, setTransactions]=useState();
+
+  useEffect(() => { 
+    api.get('transactions')    
+    .then(
+      response => console.log(response.data))
+  },[]);
+
+
+
   return(
     <Container>
       <table>
