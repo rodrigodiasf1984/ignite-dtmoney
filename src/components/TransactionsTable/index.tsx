ec8d8ce46e7af80f4ba8f288fc78ccbffbb9e028
Rodrigo Dias De Figueiredo <rodrigodiasf@gmail.com>
2021-04-26 22:27:22 +0200

    🎨 M- get data from api, format them and shows on table

--- src/components/TransactionsTable/index.tsx
+++ src/components/TransactionsTable/index.tsx
@@ -2,13 +2,22 @@ import React, { useEffect, useState } from "react";
 import { api } from "../../services/api";
 import { Container } from "./styles";
 
+interface TransactionTableProps{
+  id: number;
+  title: string; 
+  amount: number; 
+  type: string; 
+  category: string;
+  createdAt: string;
+}
+
 export function TransactionsTable() {
-  const [transactions, setTransactions]=useState();
+  const [transactions, setTransactions]=useState<TransactionTableProps[]>([]);
 
   useEffect(() => { 
     api.get('transactions')    
     .then(
-      response => console.log(response.data))
+      response => setTransactions(response.data.transactions))
   },[]);
 
 
@@ -25,18 +34,22 @@ export function TransactionsTable() {
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td>Desenvolvimento de website</td>
-            <td className="deposit">R$12.000</td>
-            <td>Desenvolvimento</td>
-            <td>20/02/2021</td>
-          </tr>
-          <tr>
-            <td>Aluguel</td>
-            <td className="withdraw">- R$1.000</td>
-            <td>Casa</td>
-            <td>20/02/2021</td>
-          </tr>         
+          {transactions.map((transaction)=>(
+          <tr key={transaction.id}>
+            <td>{transaction.title}</td>
+            <td className={transaction.type}>
+              {new Intl.NumberFormat('pt-BR', {
+                style: 'currency', 
+                currency: 'BRL'
+              } 
+              ).format(transaction.amount)}
+            </td>
+            <td>{transaction.category}</td>
+            <td>{new Intl.DateTimeFormat('pt-BR').format(
+              new Date(transaction.createdAt)
+            )}</td>
+          </tr>           
+          ))}
         </tbody>
       </table>
     </Container>
