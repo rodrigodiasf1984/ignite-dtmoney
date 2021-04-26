5938404b696fcbf1d11536f30516f6ab64db5a66
Rodrigo Dias De Figueiredo <rodrigodiasf@gmail.com>
2021-04-26 22:26:46 +0200

    🎨 M- save data

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -4,6 +4,8 @@ import {Container, TransactionTypeContainer, RadioBox} from './styles';
 import closeImg from '../../assets/close.svg';
 import incomeImg from '../../assets/income.svg';
 import outcomeImg from '../../assets/outcome.svg';
+import {api} from '../../services/api';
+
 interface NewTransactionModalProps{
   isOpen: boolean; 
   onRequestClose: ()=>void;
@@ -17,6 +19,15 @@ export function NewTransactionModal( {isOpen, onRequestClose}:NewTransactionModa
 
   const handleCreateNewTransaction = (event:FormEvent) => {
     event?.preventDefault();
+
+    const data = {
+      title, 
+      value, 
+      category, 
+      type
+    };
+
+    api.post('/transactions', data)
   }
 
   return(
