83ec19dd8045030f610d578ff929aa344ef4b8e8
Rodrigo Dias De Figueiredo <rodrigodiasf@gmail.com>
2021-04-27 23:33:39 +0200

    🎨 M- Get the transactions from the context and reduce the amounts to be displayed dynamically

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -1,32 +1,60 @@
-import React from "react";
+
 import { Container } from "./styles";
 import incomeImg from '../../assets/income.svg';
 import outcomeImg from '../../assets/outcome.svg';
 import totalImg from '../../assets/total.svg';
+import { formatCurrency } from "../../utils/formatCurrency";
+import { useTransactions } from "../../hooks/useTransactions";
 
-export function Summary(){
+export function Summary() {
+  const {transactions} = useTransactions();
+
+  const summary = transactions.reduce((acc, transaction) => {
+
+    if(transaction.type === 'deposit'){
+      acc.deposits += transaction.amount;
+      acc.total +=transaction.amount;
+    }else{
+      acc.withDraws += transaction.amount
+      acc.total -= transaction.amount;
+    }
+
+    return acc;
+  },{
+    deposits: 0, 
+    withDraws: 0, 
+    total: 0
+  });
+
   return(
-    <Container>
+    <Container>      
       <div>
         <header>
           <p>Entradas</p>
           <img src={incomeImg} alt="Entradas"/>
         </header>
-        <strong>R$1000,00</strong>
+        <strong>
+          {formatCurrency(summary.deposits)}
+        </strong>
       </div>
       <div>
         <header>
           <p>Saídas</p>
           <img src={outcomeImg} alt="Entradas"/>
         </header>
-        <strong>R$500,00</strong>
+        <strong>
+          -
+          {formatCurrency(summary.withDraws)}
+        </strong>
       </div>
       <div className="highlight-background">
         <header>
           <p>Total</p>
           <img src={totalImg} alt="Entradas"/>
         </header>
-        <strong>R$500,00</strong>
+        <strong>
+          {formatCurrency(summary.total)}
+        </strong>
       </div>
     </Container>
   )
