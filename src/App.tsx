697278f706c8e64b7a091f310777ffe05b71a119
Rodrigo Dias De Figueiredo <rodrigodiasf@gmail.com>
2021-04-27 23:31:18 +0200

    🎨 M- added transactions provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import {NewTransactionModal} from './components/NewTransactionModal';
 import Modal from 'react-modal';
 import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
 import { GlobalStyle } from "./styles/global";
+import {TransactionsProvider } from "./hooks/useTransactions";
 
 Modal.setAppElement('#root');
 
@@ -16,13 +17,14 @@ export function App() {
   const handleCloseNewTransactionModal=()=>{
     setIsNewTransactionModalOpen(false);
   }
+
+  
   return (    
-    <>
+    <TransactionsProvider>
       <Header onOpenNewTransactionModal={handleOpenNewTransactionModal}/>
       <Dashboard/>
       <NewTransactionModal isOpen={isNewTransactionModalOpen} onRequestClose={handleCloseNewTransactionModal}  />
-      <GlobalStyle/>
-      <h1>Hello World</h1>
-    </>
+      <GlobalStyle/>     
+    </TransactionsProvider>
   );
 }
