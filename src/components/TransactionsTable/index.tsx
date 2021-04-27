7a1d495d2b5e4ecedcc72b0bcef2c449a35e29d1
Rodrigo Dias De Figueiredo <rodrigodiasf@gmail.com>
2021-04-27 23:34:28 +0200

    🎨 M- added interface to define the types and call the function formatCurrency

--- src/components/TransactionsTable/index.tsx
+++ src/components/TransactionsTable/index.tsx
@@ -1,26 +1,10 @@
-import React, { useEffect, useState } from "react";
-import { api } from "../../services/api";
+
+import { useTransactions } from "../../hooks/useTransactions";
+import { formatCurrency } from "../../utils/formatCurrency";
 import { Container } from "./styles";
 
-interface TransactionTableProps{
-  id: number;
-  title: string; 
-  amount: number; 
-  type: string; 
-  category: string;
-  createdAt: string;
-}
-
 export function TransactionsTable() {
-  const [transactions, setTransactions]=useState<TransactionTableProps[]>([]);
-
-  useEffect(() => { 
-    api.get('transactions')    
-    .then(
-      response => setTransactions(response.data.transactions))
-  },[]);
-
-
+  const {transactions} = useTransactions();
 
   return(
     <Container>
@@ -38,11 +22,7 @@ export function TransactionsTable() {
           <tr key={transaction.id}>
             <td>{transaction.title}</td>
             <td className={transaction.type}>
-              {new Intl.NumberFormat('pt-BR', {
-                style: 'currency', 
-                currency: 'BRL'
-              } 
-              ).format(transaction.amount)}
+            {formatCurrency(transaction.amount)}             
             </td>
             <td>{transaction.category}</td>
             <td>{new Intl.DateTimeFormat('pt-BR').format(
